53b7b081dc5c6cdd60b599c255b35815b1074478
raphaelcamar <raphaelcamar@outlook.com>
2020-10-27 21:53:35 -0300

    End

---
+++ frontend/src/app/directives/for.directive.ts
@@ -0,0 +1,23 @@
+import { findReadVarNames } from '@angular/compiler/src/output/output_ast';
+import { Directive, Input, OnInit, TemplateRef, ViewChild, ViewContainerRef } from '@angular/core';
+
+@Directive({
+  selector: '[myFor]'
+})
+export class ForDirective implements OnInit{
+
+  @Input('myForEm') numbers: number[]
+
+  constructor(private container : ViewContainerRef, private template : TemplateRef<any>) {
+    console.log('MyFor')
+    
+   }
+
+  ngOnInit() :void{
+    for(let number of this.numbers){
+      this.container.createEmbeddedView(this.template, { $implicit: number })
+    }
+    console.log(this.numbers)
+  }
+
+}
